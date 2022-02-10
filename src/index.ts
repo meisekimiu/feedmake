83d4bec4d9d1232ebf8f2e19e416613bd9cb1882
Natalie Martin <nmartin2473@gmail.com>
2022-02-09 19:29:21 -0700

    Add argparse

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 #!/usr/bin/env ts-node
+import { ArgumentParser } from 'argparse';
+import {
+  version as PackageVersion,
+  description as PackageDescription,
+} from '../package.json';
+
+const parser = new ArgumentParser({
+  description: PackageDescription,
+});
+
+parser.add_argument('-v', '--version', {
+  action: 'version',
+  version: PackageVersion,
+});
+parser.add_argument('repo', { help: "Path to site's git repo" });
+
+console.dir(parser.parse_args());
